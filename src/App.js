de2168888bbf3e9667834cc73a064c407eb9bb96
Yakapi <realitygameserver@gmail.com>
2023-03-20 10:56:44 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 
 import Home from "./component/page/home"
-import PrivateChecker from './component/checker/private_checker'
-import Private from './component/page/private'
 
 
 function App() {
@@ -11,9 +9,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/private" element={<PrivateChecker/>}>
-          <Route path="/private/content-manager" element={<Private/>} />
-        </Route>
       </Routes>
     </div>
   );
