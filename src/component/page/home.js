a6d28a39dc32c4f9a7919534c336fd0aa03b99b5
Yakapi <realitygameserver@gmail.com>
2023-03-20 10:58:26 +0100

    update

--- src/component/page/home.js
+++ src/component/page/home.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import Navigation from '../navigation/navigation'
 
 class Home extends React.Component{
   render() {
     return (
       <div>
-        <Navigation />
         <h1>Page Home</h1>
       </div>
     );
